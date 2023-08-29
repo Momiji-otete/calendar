2d9db8d764a14f23c8ab0a0017ba89ffc97a0a6a
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-29 03:11:32 +0000

    [Add]Planモデルの追加

---
+++ db/migrate/20230829030145_create_plans.rb
@@ -0,0 +1,11 @@
+class CreatePlans < ActiveRecord::Migration[6.1]
+  def change
+    create_table :plans do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :body
+      t.datetime :date_time
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_29_021919) do
+ActiveRecord::Schema.define(version: 2023_08_29_030145) do
+
+  create_table "plans", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.datetime "date_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
